dc1157d485af2647e275659c2e4d1d939f86485f
Yen Chih Lin <a200770528@gmail.com>
2018-10-17 17:11:47 +0800

    I created a class named LBeacon to save geometry data.

---
+++ RevitAddin/LBeacon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitAddin
+{
+    class LBeacon
+    {
+        public LBeacon(FamilyInstance fi)
+        {
+            Parameter mark = fi.LookupParameter("Mark");
+            if (mark != null)
+            {
+                TaskDialog.Show("Mark", GetParameterValue(mark));
+            }
+        }
+
+        public string Mark
+        {
+            get; private set;
+        }
+
+        public string GUID
+        {
+            get; private set;
+        }
+
+        public string BeaconType
+        {
+            get; private set;
+        }
+
+        public double XLocation
+        {
+            get; private set;
+        }
+
+        public double YLocation
+        {
+            get; private set;
+        }
+
+        public string Level
+        {
+            get; private set;
+        }
+
+        private string GetParameterValue(Parameter parameter)
+        {
+            switch (parameter.StorageType)
+            {
+                case StorageType.Double:
+                    return parameter.AsValueString();
+                case StorageType.ElementId:
+                    return parameter.AsElementId().IntegerValue.ToString();
+                case StorageType.Integer:
+                    return parameter.AsValueString();
+                case StorageType.None:
+                    return parameter.AsValueString();
+                case StorageType.String:
+                    return parameter.AsString();
+                default:
+                    return "";
+            }
+        }
+    }
+}
